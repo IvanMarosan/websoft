54ccb3c37667b75cc065ea2b419aed6f1d65c9a5
Ivan Marosan <marosan.ivan@gmail.com>
2020-01-27 13:51:22 +0100

    forgot to save the details

---
+++ work/report/js/duck.js
@@ -0,0 +1,20 @@
+"use strict";
+
+(function() {
+
+  var element = document.getElementByID("duck")
+
+  element.addEventListener("click", function() {
+    element.innerHTML = element.innerHTML + "<p> QUACK </p>";
+    Comments.log("Duck clicked!");
+  });
+
+  element.addEventListener("mouseover", function() {
+    element.style.left = element.OffsetLeft + 20 + "px";
+    console.log(element.style.left);
+    console.log(element.OffsetLeft);
+    console.log("Mouse Over");
+  });
+  console.log(element);
+  console.log("Duck ready!")
+})();
