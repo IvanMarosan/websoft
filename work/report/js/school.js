3fed6ca4cd6be2614f4c88917eeb6a720ba82418
Ivan Marosan <marosan.ivan@gmail.com>
2020-02-12 10:40:08 +0100

    Test code

--- work/report/js/school.js
+++ work/report/js/school.js
@@ -1,9 +1,13 @@
-"use strict"
+"use strict";
 
-fetch('https://api.scb.se/UF0109/v2/skolenhetsregister/sv/skolenhet')
-    .then((response) => {
-    return response.json();
-})
-.then((myJson) => {
-    console.log(myJson);
-})
+
+function getData(datalabel = "1281.json") {
+
+    let test = fetch(datalabel).then((respone) => {
+        return respone.json();}).then((myJson) => {
+            console.log(myJson);
+            return myJson;
+});
+    console.log("Data was fetched")
+    return test;
+}
