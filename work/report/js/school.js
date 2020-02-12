4c80198a6cf25cc993f3c1052f7e185a51946741
Ivan Marosan <marosan.ivan@gmail.com>
2020-02-12 12:21:33 +0100

    hotfix

--- work/report/js/school.js
+++ work/report/js/school.js
@@ -2,14 +2,13 @@
     'use strict';
 
     let url;
-    
+
     url = "data/1281.json";
-    fetch(url)
-        .then((response) => {
-            return response.json();
-        })
-        .then((myJson) => {
-            console.log(myJson);
-        });
-    console.log('Console is ready!');
+    fetch(url).then((respone) => {
+        return respone.json();
+    }).then((myJson) => {
+    console.log(myJson);
+    });
+
+    console.log("Console is ready");
 })();
