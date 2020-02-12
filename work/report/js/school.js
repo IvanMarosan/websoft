8145f828ece698ffb4c420c783c69b8658bdd0cf
Ivan Marosan <marosan.ivan@gmail.com>
2020-02-12 12:30:52 +0100

    trying out new code

--- work/report/js/school.js
+++ work/report/js/school.js
@@ -1,14 +1,26 @@
-(function () {
-    'use strict';
+var myList = document.querySelector('ul');
 
-    let url;
-
-    url = "data/1281.json";
-    fetch(url).then((respone) => {
-        return respone.json();
-    }).then((myJson) => {
-    console.log(myJson);
+fetch('data/1281.json')
+    .then(function(response) {
+        if (!response.ok) {
+            throw new Error("HTTP error, status = " + response.status);
+        }
+        return response.json();
+    })
+    .then(function(json) {
+        for(var i = 0; i < json.Skolenheter.length; i++) {
+            var listItem = document.createElement('li');
+            listItem.innerHTML = '<strong>' + json.Skolenheter[i].Skolenhetskod + '</strong>';
+            listItem.innerHTML = '<strong>' + json.Skolenheter[i].Skolenhetsnamn + '</strong>';
+            listItem.innerHTML = '<strong>' + json.Skolenheter[i].Kommunkod + '</strong>';
+            listItem.innerHTML = '<strong>' + json.Skolenheter[i].PeOrgNr + '</strong>';
+            myList.appendChild(listItem);
+        }
+    })
+    .catch(function(error) {
+        var p = document.createElement('p');
+        p.appendChild(
+            document.createTextNode('Error: ' + error.message)
+        );
+        document.body.insertBefore(p, myList);
     });
-
-    console.log("Console is ready");
-})();
