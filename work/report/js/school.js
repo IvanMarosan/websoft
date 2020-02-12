b74e9b39f8b734de9120df4504cc04969ca8c680
Ivan Marosan <marosan.ivan@gmail.com>
2020-02-12 10:51:43 +0100

    Updating assignemnt 3

--- work/report/js/school.js
+++ work/report/js/school.js
@@ -1,13 +1,26 @@
-"use strict";
+(function () {
+    'use strict';
 
+    let url;
 
-function getData(datalabel = "1281.json") {
-
-    let test = fetch(datalabel).then((respone) => {
-        return respone.json();}).then((myJson) => {
+    //url = "https://api.scb.se/UF0109/v2/skolenhetsregister/sv/kommun/1081";
+    url = "data/1281.json";
+    fetch(url)
+        .then((response) => {
+            return response.json();
+        })
+        .then((myJson) => {
             console.log(myJson);
-            return myJson;
-});
-    console.log("Data was fetched")
-    return test;
-}
+        });
+
+    url = "https://rem.dbwebb.se/api/users";
+    fetch(url)
+        .then((response) => {
+            return response.json();
+        })
+        .then((myJson) => {
+            console.log(myJson);
+        });
+
+    console.log('Console is ready!');
+})();
